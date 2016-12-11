41a8f92f94c2844bcae3620aabe097a6d8716d14
rokyfox <sorace.n@gmail.com>
2016-12-11 13:22:50 +0000

    Added Polybar config, and various changes.

--- lock.sh
+++ lock.sh
@@ -4,7 +4,8 @@ cd "/home/nicola/.config/i3/"
 
 import -silent -window root lock.png;
 notify-send --urgency low "Locking..."
-convert lock.png -blur 0x10 lock.png;
+convert lock.png -scale 25% -blur 0x20 -scale 400% lock.png;
+#convert lock.png -scale 10% -scale 1000% lock.png;
 composite -gravity center pad.png lock.png lock.png;
 
 i3lock -i lock.png --textcolor=eee8d5ff --ringcolor=07364295 --keyhlcolor=268bd295 --bshlcolor=dc322f95 --insidecolor=002b3695 --insidevercolor=268bd295 --ringvercolor=268bd295 --insidewrongcolor=dc322f95 --ringwrongcolor=dc322f95 --linecolor=07364295 --separatorcolor=07364295;
